04efa04e8ec625845bc7324585bb5b978f40762e
Daniel Gakwaya <wayadn88@gmail.com>
2025-05-30 10:03:09 +0300

    Add project 05_QStringListModel

---
+++ 05_QStringListModel/ui_widget.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'widget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QListView,
+    QSizePolicy, QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(796, 528)
+        self.horizontalLayout = QHBoxLayout(Widget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.listView = QListView(Widget)
+        self.listView.setObjectName(u"listView")
+
+        self.horizontalLayout.addWidget(self.listView)
+
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.label)
+
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"QStringListModel", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"Some label", None))
+    # retranslateUi
+
---
+++ 05_QStringListModel/widget.py
@@ -0,0 +1,42 @@
+from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QColor, QPixmap
+from PySide6.QtCore import Slot, QModelIndex, QStringListModel, Qt
+from ui_widget import Ui_Widget
+
+
+class Widget(QWidget): 
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        # Connect to the ui form
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
+
+        # Get all available color names 
+        self.color_list = QColor.colorNames()
+
+        # Create a string list model with the color names
+        self.model = QStringListModel(self.color_list,self)
+
+        # Tell the list view to display the string list model
+        self.ui.listView.setModel(self.model)
+
+        # Connect the clicked signal to our slot
+        self.ui.listView.clicked.connect(self.view_clicked)
+
+
+    @Slot(QModelIndex)
+    def view_clicked(self,index):
+        # Get the color name from the model
+        color_name = self.model.data(index,Qt.DisplayRole)
+
+        # Create a pixmap filled with the selected color
+        pixmap = QPixmap(self.ui.label.size())
+        pixmap.fill(QColor(color_name))
+
+        # Set the pixmap to the label
+        self.ui.label.setPixmap(pixmap)
+
+        # Print all the colors
+        print("All colors", self.model.stringList())
+
